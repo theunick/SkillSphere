960e5e0b17f3c45e7ed8b6faa40e66720c6b67f2
Giogioatti <giogioatti@gmail.com>
2024-05-19 17:58:40 +0200

    inizio corso

---
+++ db/migrate/20240519155703_create_courses.rb
@@ -0,0 +1,12 @@
+class CreateCourses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :courses do |t|
+      t.string :title
+      t.string :code
+      t.string :category
+      t.references :seller, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,26 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2024_05_19_155703) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.string "code"
+    t.string "category"
+    t.integer "seller_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["seller_id"], name: "index_courses_on_seller_id"
+  end
+
+  add_foreign_key "courses", "sellers"
+end
