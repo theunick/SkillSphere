c054f6143d6564f8903ee451899d8353ad32b110
nicolas leone <leone.1986354@studenti.uniroma1.it>
2024-06-25 21:19:15 +0200

    prima homepage test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root 'courses#index'
-  resources :accounts
+  get 'home/index'
+  root 'home#index'
   resources :courses
-  resources :homepage
+  resources :accounts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
