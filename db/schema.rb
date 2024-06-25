c929ce70772b2f1abe77fe0ea36226c4c944e024
nicolas leone <leone.1986354@studenti.uniroma1.it>
2024-06-25 20:19:46 +0200

    inizio account

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_19_162808) do
+ActiveRecord::Schema.define(version: 2024_06_25_180734) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "surname"
+    t.integer "role"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
