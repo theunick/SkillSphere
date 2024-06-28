a1aaa5b8079842fa110a66c25e75bc44e8e7bd82
Giogioatti <giogioatti@gmail.com>
2024-06-28 11:00:31 +0200

    quasi finita google auth

--- db/migrate/20240628084244_add_omniauth_columns_to_accounts.rb
+++
@@ -1,8 +0,0 @@
-class AddOmniauthColumnsToAccounts < ActiveRecord::Migration[6.1]
-  def change
-    add_column :accounts, :provider, :string
-    add_column :accounts, :image, :string
-    add_column :accounts, :oauth_token, :string
-    add_column :accounts, :oauth_expires_at, :datetime
-  end
-end
