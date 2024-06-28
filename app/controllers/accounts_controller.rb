050df8ef2a03f9f6e4e4fbdc5e603520bb2185e2
nicolas leone <leone.1986354@studenti.uniroma1.it>
2024-06-28 07:54:24 +0200

    patch generale 2

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,71 +1,95 @@
 class AccountsController < ApplicationController
-    before_action :set_account, only: %i[show edit update destroy]
+  before_action :set_account, only: %i[show edit update destroy]
   
-    def index
-      @accounts = Account.all
-      @assistance = Assistance.new
+  def index
+    @accounts = Account.all
+    @assistance = Assistance.new
+  end
+  
+  def show
+    @account = Account.find(params[:id])
+  end
+  
+  def new
+    @account = Account.new
+  end
+  
+  def edit
+  end
+  
+  def create
+    @account = Account.new(account_params)
+  
+    if @account.save
+      redirect_to @account, notice: 'Account was successfully created.'
+    else
+      render :new
     end
+  end
   
-    def show
-      @account = Account.find(params[:id])
+  def become_seller
+    if @account.update(role: 1)
+      redirect_to accounts_path, notice: 'Account has been updated to seller.'
+    else
+      redirect_to accounts_path, alert: 'Failed to update account.'
     end
+  end
   
-    def new
-      @account = Account.new
-    end
+  def destroy
+    @account.destroy
+    redirect_to accounts_url, notice: 'Account was successfully destroyed.'
+  end
   
-    def edit
-    end
+  private
   
-    def create
-      @account = Account.new(account_params)
+  def set_account
+    @account = Account.find(params[:id])
+  end
   
-      if @account.save
-        redirect_to @account, notice: 'Account was successfully created.'
-      else
-        render :new
-      end
-    end
-  
-    def update
-      if @account.update(account_params)
-        redirect_to @account, notice: 'Account was successfully updated.'
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
-      @account.destroy
-      redirect_to accounts_url, notice: 'Account was successfully destroyed.'
-    end
-  
-    private
-  
-    def set_account
-      @account = Account.find(params[:id])
-    end
-  
-    def account_params
-      params.require(:account).permit(:email, :name, :surname, :role)
-    end
+  def account_params
+    params.require(:account).permit(:email, :name, :surname, :role)
+  end
 
-    def assistance
-      @assistance = Assistance.new
-    end
+  def assistance
+    @assistance = Assistance.new
+  end
   
-    def create_assistance
-      @assistance = current_user.assistances.build(assistance_params)
-      if @assistance.save
-        redirect_to account_path, notice: 'Assistance request submitted successfully.'
-      else
-        render :index
-      end
+  def create_assistance
+    @assistance = current_user.assistances.build(assistance_params)
+    if @assistance.save
+      redirect_to account_path, notice: 'Assistance request submitted successfully.'
+    else
+      render :index
     end
+  end
   
-    private
+  private
   
-    def assistance_params
-      params.require(:assistance).permit(:message)
+  def assistance_params
+    params.require(:assistance).permit(:message)
+  end
+
+  def become_customer
+    if @account.update(role: 0)
+      redirect_to accounts_path, notice: 'Account has been updated to customer.'
+    else
+      redirect_to accounts_path, alert: 'Failed to update account.'
     end
+  end
+    
+  def become_seller
+    if @account.update(role: 1)
+      redirect_to accounts_path, notice: 'Account has been updated to seller.'
+    else
+      redirect_to accounts_path, alert: 'Failed to update account.'
+    end
+  end
+
+  def become_admin
+    if @account.update(role: 2)
+      redirect_to accounts_path, notice: 'Account has been updated to admin.'
+    else
+      redirect_to accounts_path, alert: 'Failed to update account.'
+    end
+  end
 end  
